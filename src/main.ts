8bd7f6c5f8437a1d6ddf494acc4b7bfb7ae0df7a
Tomas Garcia <tomasotano25@gmail.com>
2022-10-13 15:18:00 -0400

    Clase 06 add api key

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ interface Response {
 }
 
 const API_URL = "https://api.thedogapi.com/v1/images/search";
+const API_KEY =
+  "live_IVjVVgYeA58x2gmhfJFPm2GR24g5jk8N6FHHuPLyjeC7AKKHo9NQKvjqEyx0fAfs";
 
 const images = document.querySelector<HTMLDivElement>("#images");
 const btnRefresh = document.querySelector<HTMLButtonElement>("#refresh");
@@ -26,7 +28,9 @@ async function fetchData(urlApi: string): Promise<Response[]> {
 async function reload() {
   images!.innerHTML = "";
 
-  const imagesSrc: Response[] = await fetchData(`${API_URL}?limit=3`);
+  const imagesSrc: Response[] = await fetchData(
+    `${API_URL}?limit=3&api_key=${API_KEY}&`
+  );
 
   imagesSrc.forEach((item) => {
     const image = document.createElement("img");
